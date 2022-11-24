74947d7eab093d3625120ce4d46e80df687efad0
DevDanielOropeza <69995760+DevDanielOropeza@users.noreply.github.com>
2022-11-24 13:50:09 -0600

    Initial commit
    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useRef } from "react";
 
 function App() {
+
+  const nombreRef = useRef("");
+  const apellidosRef = useRef("");
+  const usuarioRef = useRef("");
+  const emailRef = useRef("");
+
+  const onsubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post("/users", {
+      name: `${nombreRef.current.value} ${apellidosRef.current.value}`,
+      username: usuarioRef.current.value,
+      email: emailRef.current.value
+    }, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    const { id, name, username } = res.data;
+
+    // Respuesta con nuevo usuario...
+    const newEmployeeUser = { id, name, username };
+    console.log(newEmployeeUser);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onsubmit}>
+        <input type="text" name="nombre" ref={nombreRef} placeholder="Nombre" />
+        <input type="text" name="apellidos" ref={apellidosRef} placeholder="Apellidos" />
+        <input type="text" name="nombreUsuario" ref={usuarioRef} placeholder="Nombre de usuario" />
+        <input type="text" name="correo" ref={emailRef} placeholder="Correo electrónico" />
+        <input type="submit" value="Insertar" />
+      </form>
     </div>
   );
 }
